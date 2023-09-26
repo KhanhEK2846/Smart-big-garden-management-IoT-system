d6736ec4e9a96295164346235429638d700b646c
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2023-09-26 19:48:44 +0700

    Merge branch 'main' of https://github.com/KhanhEK2846/Smart-big-garden-management-IoT-system

--- src/main.cpp
+++ src/main.cpp
@@ -1834,6 +1834,7 @@ String MACAddressCovert(uint8_t* mac)// Convert unit8_t to String MAC
     sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0],mac[1], mac[2],mac[3],mac[4],mac[5]);
     // Serial.print("MAC Convert: ");
     // Serial.println(String(macStr));
+
     return String(macStr);
 }
 void RefreshNodeIP(String locate = "0")
