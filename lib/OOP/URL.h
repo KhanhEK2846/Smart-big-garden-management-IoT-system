0e431ba675ee08429ef70e739b7dd22eba56217f
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2023-10-17 07:35:54 +0700

    Add URL.h 	new file:   lib/OOP/URL.h

---
+++ lib/OOP/URL.h
@@ -0,0 +1,15 @@
+//Firebase Server
+#define API_KEY "AIzaSyCs-_UEbcWTW9ANFJ-igucZPCbS7XclUIk"
+#define DATABASE_URL "https://gradentiots-default-rtdb.firebaseio.com/"
+#define Database_Secrets "5jR3TXfdANKSI6xXUgBiJc3wvojE26hqcY3bVNCS"
+//PING Server
+#define PINGInternet "www.google.com"
+//NTP Server
+#define NTPserver1 "pool.ntp.org"
+#define NTPserver2 "asia.pool.ntp.org"
+#define NTPserver3 "oceania.pool.ntp.org"
+//MQTT Server
+#define MQTT_SERVER "test.mosquitto.org"
+#define MQTT_PORT 1883
+#define MQTT_Pump_TOPIC "GardenIoT/Pump"
+#define MQTT_LED_TOPIC "GardenIoT/LED"
