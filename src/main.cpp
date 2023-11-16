baa95f23abc8a6ed213420c7885b846efe0b155a
KhanhEK2846 <lehoangkhanh2846912002@gmail.com>
2023-11-16 18:04:28 +0700

    Init Function LoRa

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,11 @@
 #define Soil_Moisture 2 //Read Soild Sensor
 #define Pumps 4 // Control Pump
 #define Light 5 //Control Light
+//SPI Port
+#define CS_Port 14
+#define CLK_Port 15
+#define MISO_Port 17
+#define MOSI_Port 16
 //DHT11 Variable
 #define DHTTYPE DHT22 
 DHT dht(DHTPIN, DHTTYPE);
@@ -907,6 +912,44 @@ void Init_Server() // FIXME: Fix backend server
   server.begin();
 }
 #pragma endregion
+#pragma region LoRa
+void Init_LoRa()
+{
+  LoRa.setPins();
+  while (!LoRa.begin(866E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+}
+void SendLoRa(String data)
+{
+  LoRa.beginPacket();
+  LoRa.print(data);
+  LoRa.endPacket();
+}
+String ReceiveLoRa()
+{
+  int packetSize = LoRa.parsePacket();
+    if (packetSize) 
+    {
+      // received a packet
+      Serial.print("Received packet '");
+
+      // read packet
+      while (LoRa.available()) {
+        String LoRaData = LoRa.readString();
+        Serial.print(LoRaData); 
+      }
+
+      // print RSSI of packet
+      Serial.print("' with RSSI ");
+      Serial.println(LoRa.packetRssi());
+    }
+}
+
+#pragma endregion LoRa
 #pragma region Send Message
 void PrepareMess() //Decide what to send
 {
@@ -1255,7 +1298,7 @@ void setup()
 }
 void loop() 
 {
-  Auto();
-  Network();
+  //Auto();
+  //Network();
 }
 #pragma endregion
